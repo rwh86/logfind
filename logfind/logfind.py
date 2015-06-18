05c46843dac49281ea186be4e9701a3ca48511c0
Robert Hutton <robert.hutton@unimelb.edu.au>
2015-06-18 17:58:24 +1000

    Read command line args and config file, convert to list of files

--- logfind/logfind.py
+++ logfind/logfind.py
@@ -3,10 +3,37 @@
 """
 Logfind: implementation of http://projectsthehardway.com/2015/06/16/project-1-logfind-2/
 """
-
+import os
 import sys
+import glob
 
-for arg in sys.argv:
-    print arg
+def main():
+    sys.argv.pop(0)
+
+    conffile = os.environ['HOME'] + '/.logfind'
+    conf = open(conffile, 'r')
+
+    regexps = []
+    for line in conf:
+        regexps.append(line.rstrip())
+    #print regexps
+
+    strings = []
+    for arg in sys.argv:
+        strings.append(arg)
+    #print strings
+
+    files = regexps_to_files(regexps[:])
+
+# convert regexps in the config file to a list of log files to process
+def regexps_to_files(regexps):
+    files = []
+
+    for regexp in regexps:
+        files += glob.glob(regexp)
+
+    print files
+
+main()
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
