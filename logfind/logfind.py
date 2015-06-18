cb7cc43ad22b5cfa86acd81fb29e42e6814fdfe3
Robert Hutton <robert.hutton@unimelb.edu.au>
2015-06-18 11:54:17 +1000

    Read command line args

---
+++ logfind/logfind.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""
+Logfind: implementation of http://projectsthehardway.com/2015/06/16/project-1-logfind-2/
+"""
+
+import sys
+
+for arg in sys.argv:
+    print arg
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
